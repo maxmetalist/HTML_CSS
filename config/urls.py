0e4dfd25b28de0cd8dd265010535e5f2a1e7b36e
Max <z.max83@mail.ru>
2025-08-10 19:05:27 +0300

    коммит на домашку по fbv и cbv

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
+    path("blog/", include("blog.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
