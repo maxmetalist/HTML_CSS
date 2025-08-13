607d3a069b059dc3570189014669e56b47b82dda
Max <z.max83@mail.ru>
2025-08-13 23:14:37 +0300

    коммит на домашку по формам

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import HomeView, OurContactsView, ContactsView, ContactsSuccessView, CatalogView, ProductDetailView
+from catalog.views import HomeView, OurContactsView, ContactsView, ContactsSuccessView, CatalogView, ProductDetailView, \
+    ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
@@ -12,4 +13,8 @@ urlpatterns = [
     path("contacts_success/<str:name>/", ContactsSuccessView.as_view(), name="contacts_success"),
     path("our_contacts/", OurContactsView.as_view(), name="our_contacts"),
     path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
+
+from catalog.forms import ProductForm
 from catalog.models import Contact, Product
 
 
 class HomeView(TemplateView):
     """Контроллер страницы home с выводом последних 5 продуктов
        и популярными товарами"""
-    template_name = "home.html"
+    template_name = "catalog/home.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["latest_products"] = Product.objects.order_by("-created_at")[:5]
@@ -18,7 +21,7 @@ class HomeView(TemplateView):
 
 class OurContactsView(TemplateView):
     """Контроллер для отображения контактной информации"""
-    template_name = "contacts.html"
+    template_name = "catalog/contacts.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         try:
@@ -31,7 +34,7 @@ class OurContactsView(TemplateView):
 
 class ContactsView(View):
     """Класс для обработки контактной формы"""
-    template_name = "contacts.html"
+    template_name = "catalog/contacts.html"
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
@@ -56,11 +59,48 @@ class ContactsSuccessView(View):
 class CatalogView(ListView):
     """Контроллер для отображения каталога товаров"""
     model = Product
-    template_name = "catalog.html"
+    template_name = "catalog/catalog.html"
     context_object_name = "products"
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Product.objects.order_by('name', 'created_at')
+
 
 class ProductDetailView(DetailView):
     """Контроллер для отображения информации об отдельном товаре"""
     model = Product
-    template_name = "product_detail.html"
+    template_name = "catalog/product_detail.html"
     context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+    def form_valid(self, form):
+        # Дополнительные действия перед сохранением
+        return super().form_valid(form)
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    context_object_name = 'product'
+
+    def get_success_url(self):
+        return reverse_lazy('product_detail', kwargs={'pk': self.object.pk})
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
+    context_object_name = 'product'
